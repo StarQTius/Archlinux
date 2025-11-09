bea0659f43e69733ffae97b0fc858a0d15c52d9b
StarQTius <dussautoirp@gmail.com>
2025-11-09 14:27:20 +0100

    Automatic backup

--- bin/config.fish/keyring.sh
+++ bin/config.fish/keyring.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
 
-if [ -z "$GNOME_KEYRING_CONTROL" ]; then
-  eval $(dbus-launch --sh-syntax)
-  eval $(echo "" | gnome-keyring-daemon --unlock --components=secrets 2>/dev/null)
-  export GNOME_KEYRING_CONTROL
-fi
