b8e661421eab3ce812117a47d9bd0b15421b281f
StarQTius <starqtius@proton.me>
2026-09-12 13:35:47 +0200

    Automatic backup

--- bin/ui/menu.fish
+++ bin/ui/menu.fish
@@ -7,11 +7,25 @@ if test -n "$(pgrep --exact $this_script --ignore-ancestors)"
   exit 1
 end
 
-echo $PATH \
-  | xargs --delimiter=" " --replace=% fish -c'if test -d %; echo %; end' \
-  | xargs --replace=% find % -type f -executable -follow \
-  | xargs --max-lines=1 basename \
-  | fzf --reverse --color=bg:#000000 \
-  | xargs --no-run-if-empty which \
-  | xargs --no-run-if-empty swaymsg exec --
+set user_choice (
+  history \
+  | nl \
+  | string match --regex "^\s*[0-9]+\s*[^ ]+" \
+  | sort --key=2 --key=1n \
+  | uniq --skip-fields=1 \
+  | sort --key=1n \
+  | string replace --regex "^\s*[0-9]+\s*([^ ]+)\$" "\$1" \
+  | fzf --reverse --color="bg:#000000" --print-query \
+  | tail --lines=1
+)
 
+if test -z "$user_choice"
+  exit 1
+end
+
+if which "$user_choice"
+  history append "$user_choice"
+  swaymsg exec -- "$user_choice"
+else
+  notify-send --urgency normal "'$user_choice': command not found"
+end
